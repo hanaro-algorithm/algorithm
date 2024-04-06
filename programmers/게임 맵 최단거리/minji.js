004da19ba71850658f5a19f9be40f656e647da77
minjilee <budlalswl1129@gmail.com>
2024-04-06 19:38:26 +0900

    feat: 게임 맵 최단거리 풀이
    BFS로 효율성 실패 해결

--- programmers/게임 맵 최단거리/minji.js
+++ programmers/게임 맵 최단거리/minji.js
@@ -1,36 +1,37 @@
 function solution(maps) {
   let answer = -1;
-  let visited = Array.from({ length: maps.length }, () =>
-    new Array(maps[0].length).fill(0)
-  ); // 지나온 칸 수 체크
-
   const row = maps.length;
   const col = maps[0].length;
+  let visited = Array.from({ length: row }, () => new Array(col).fill(0)); // 지나온 칸 수 체크
 
   const dx = [0, 0, -1, 1];
   const dy = [-1, 1, 0, 0];
-  dfs(0, 0, 1); // 출발지점
 
-  // (x,y) 위치와 이동한 칸수 cnt
-  function dfs(x, y, cnt) {
-    // 목적지에 도달한 경우 리턴
-    if (x === col - 1 && y === row - 1) {
-      if (answer === -1) answer = cnt;
-      else answer = Math.min(answer, cnt);
-      return;
+  let queue = [];
+  queue.push([0, 0, 1]); // 스타트 위치
+  visited[0][0] = 1;
+
+  while (queue.length > 0) {
+    const [cx, cy, cnt] = queue.shift();
+
+    // 목적지에 도달한 경우
+    if (cx === col - 1 && cy === row - 1) {
+      answer = cnt;
+      break;
     }
-    visited[y][x] = cnt; // 방문 표시(누적합)
 
     for (let i = 0; i < 4; i += 1) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      // 범위를 벗어나는 경우
-      if (nx < 0 || nx >= col || ny < 0 || ny >= row) continue;
+      const nx = cx + dx[i];
+      const ny = cy + dy[i];
+
+      if (nx < 0 || nx >= col || ny < 0 || ny >= row) continue; // 범위 벗어나는 경우
       if (maps[ny][nx] === 1 && !visited[ny][nx]) {
-        dfs(nx, ny, cnt + 1);
+        // 방문 표시(꺼낼 때 방문 표시하면, 그 사이에 많은 값들이 들어올 수 있음)
+        visited[ny][nx] = cnt + 1;
+        // 좌표 이동
+        queue.push([nx, ny, cnt + 1]);
       }
     }
-    visited[y][x] = 0;
   }
   return answer;
 }
@@ -44,21 +45,12 @@ console.log(
     [0, 0, 0, 0, 1],
   ])
 );
-
-/**
- *  [1, 0, 9, 10, 11],
-    [2, 0, 8, 0, 12],
-    [3, 0, 7, 8, 9],
-    [4, 5, 6, 0, 10],
-    [0, 0, 0, 0, 11],
- */
-
-// console.log(
-//   solution([
-//     [1, 0, 1, 1, 1],
-//     [1, 0, 1, 0, 1],
-//     [1, 0, 1, 1, 1],
-//     [1, 1, 1, 0, 0],
-//     [0, 0, 0, 0, 1],
-//   ])
-// );
+console.log(
+  solution([
+    [1, 0, 1, 1, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 1],
+  ])
+);
