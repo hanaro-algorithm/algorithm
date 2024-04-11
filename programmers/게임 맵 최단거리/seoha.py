771e6c814e6250c06460322abdbcee36f435016a
sseohalee <seohalee0131@gmail.com>
2024-04-12 08:38:26 +0900

    feat: 게임 맵 최단거리 문제 풀이

---
+++ programmers/게임 맵 최단거리/seoha.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+
+def solution(maps):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    n = len(maps)
+    m = len(maps[0])
+
+    def bfs(x, y):
+        q = deque()
+        q.append((x, y))
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                    continue
+                if maps[nx][ny] == 0:
+                    continue
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    q.append((nx, ny))
+        return maps[n - 1][m - 1]
+
+    if (bfs(0, 0) == 1):
+        return -1
+    return maps[n - 1][m - 1]
+
+
+maps = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]
+print(solution(maps))
