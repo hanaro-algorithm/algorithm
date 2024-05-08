ac0cbd0c0ff7bc58e95c0bd95c6ff97b1ea77b1c
alswlfl29 <budlalswl1129@gmail.com>
2024-05-08 21:56:41 +0900

    [Add] 특정 거리의 도시 찾기 문제 풀이

--- baekjoon/특정 거리의 도시 찾기/minji.js
+++ baekjoon/특정 거리의 도시 찾기/minji.js
@@ -8,26 +8,33 @@ for (let i = 1; i <= M; i += 1) {
     const [A, B] = input[i].split(" ").map(Number);
     cities[A].push(B);
 }
-let shortDepth = new Array(N + 1).fill(0);
 
-let queue = [];
-queue.push([X, 0]); // [도시 번호, 거리]
+let visited = Array.from({ length: N + 1 }).fill(false); // 방문 여부
+let result = new Array(N + 1).fill(0); // 최단거리 배열
 
-while (queue.length > 0) {
-    let [city, depth] = queue.shift();
-    if (shortDepth[city] === 0 || shortDepth[city] > depth)
-        shortDepth[city] = depth;
+function dfs(city, depth) {
+    // depth가 K 이상 혹은 기존 거리길이보다 depth의 길이가 길면 dfs 수행안함
+    if (depth > K || (result[city] !== 0 && result[city] < depth)) return;
+    visited[city] = true; // 방문 표시
+    result[city] = depth; // 거리 길이 넣기
+
+    // 인접해 있는 도로로 이동
     for (let c of cities[city]) {
-        queue.push([c, depth + 1]);
+        if (!visited[c]) dfs(c, depth + 1);
     }
+    visited[city] = false;
 }
 
+dfs(X, 0);
+
 let found = false;
-shortDepth.map((d, idx) => {
-    if (d === K) {
+let answer = "";
+result.map((r, idx) => {
+    if (r === K) {
+        answer += idx + "\n";
         found = true;
-        console.log(idx);
     }
 });
+if (!found) answer += -1;
 
-if (!found) console.log(-1);
+console.log(answer);
