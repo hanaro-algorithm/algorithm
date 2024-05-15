ecb4ceba805b3c447e006bc105399b48e4d9d75f
alswlfl29 <budlalswl1129@gmail.com>
2024-05-15 17:14:51 +0900

    [Add] 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기

--- SQL/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/minji.sql
+++ SQL/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/minji.sql
@@ -1,8 +1,16 @@
-select new_c.car_id, new_c.car_type, new_c.fee
-from (select c.car_id, c.car_type, ceil(c.daily_fee*(1-p.discount_rate*0.01)) as fee
-from CAR_RENTAL_COMPANY_CAR as c, CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-where c.car_type in ('SUV', '세단') and p.car_type in ('SUV','세단') and p.duration_type = '30일 이상') as new_c
-inner join CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-on new_c.car_id = h.car_id
-where h.end_date>=date('2022-11-01') and h.start_date<=date('2022-12-01')
-order by new_c.fee desc, new_c.car_type asc, new_c.car_id;
+select c.car_id, c.car_type, round(c.daily_fee*30*(100-p.discount_rate)*0.01) as fee
+from car_rental_company_car as c
+# car_rental_company_car는 다른 두 테이블과 각각 연결되어야 함.
+join car_rental_company_rental_history as h
+on c.car_id=h.car_id
+join car_rental_company_discount_plan as p
+on c.car_type = p.car_type
+where c.car_id not in (
+    select car_id
+    from car_rental_company_rental_history
+    # 대여 가능한 날짜는 즉, 2022-11-01~2022-12-01
+    where end_date>='2022-11-01' and start_date<='2022-12-01'
+) and p.duration_type like '30%'
+group by c.car_id
+having c.car_type in ('세단', 'suv') and (fee >= 500000 and fee < 2000000)
+order by fee desc, car_type, car_id desc
