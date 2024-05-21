dafc28f19ba8428e675be18b153f434e57f586e6
minjilee <budlalswl1129@gmail.com>
2024-05-21 22:12:43 +0900

    [Add] 여행경로 문제 풀이(풀이참고)

--- programmers/여행경로/minji.js
+++ programmers/여행경로/minji.js
@@ -1,42 +1,33 @@
 function solution(tickets) {
     let answer = [];
-    let arr = {};
-    tickets.map((ticket) => {
-        if (!Object.keys(arr).includes(ticket[0])) {
-            arr[ticket[0]] = [ticket[1]];
-        } else {
-            arr[ticket[0]] = [...arr[ticket[0]], ticket[1]];
-            arr[ticket[0]].sort();
+    let result = []; // DFS를 돌면서 방문하는 역 넣음
+    let visited = []; // 방문표시
+
+    let len = tickets.length;
+
+    tickets.sort(); // 도착지를 알파벳 순으로 정렬하기 위한 정렬
+
+    const dfs = (str, count) => {
+        result.push(str); // 일단 해당역을 리스트에 삽입
+        // 모든 경로를 다 거친 경우 종료
+        if (count === len) {
+            answer = result;
+            return true;
         }
-    });
-    let startArr = Object.keys(arr);
-    let endArr = Object.values(arr);
 
-    bfs(arr, startArr, endArr, "ICN", answer);
-
-    startArr.map((item, index) => {
-        if (endArr[index].length > 0) answer.push(...endArr[index]);
-    });
-
-    return answer;
-}
-
-function bfs(arr, startArr, endArr, start, answer) {
-    answer.push(start);
-    let indexStart = startArr.indexOf(start);
-    let items = endArr[indexStart];
-    for (let item = 0; item < items.length; item += 1) {
-        if (
-            !startArr.includes(items[item]) ||
-            endArr[startArr.indexOf(items[item])].length === 0
-        )
-            continue;
-        else {
-            let end = items.splice(item, 1);
-            bfs(arr, startArr, endArr, ...end, answer);
-            break;
+        for (let i = 0; i < len; i += 1) {
+            if (!visited[i] && tickets[i][0] === str) {
+                visited[i] = true; // 현재노드 방문 처리
+                if (dfs(tickets[i][1], count + 1)) return true;
+                visited[i] = false; // 현재노드 방문처리 취소
+            }
         }
-    }
+        result.pop(); // 다른 경로로 가지 못할 경우 최근 도착지 빼기
+        return false;
+    };
+
+    dfs("ICN", 0);
+
     return answer;
 }
 
@@ -76,6 +67,5 @@ console.log(
         ["DOO", "BOO"],
         ["BOO", "ICN"],
         ["COO", "BOO"],
-        ,
     ])
 ); //["ICN", "BOO", "DOO", "BOO", "ICN", "COO", "DOO", "COO", "BOO"]
