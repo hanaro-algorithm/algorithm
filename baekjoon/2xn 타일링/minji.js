95e4e2399470e4ac6065882b3ce42fdc09dfd199
minjilee <budlalswl1129@gmail.com>
2024-05-27 20:39:13 +0900

    [Add] 타일링 문제 풀이

--- baekjoon/2xn 타일링/minji.js
+++ baekjoon/2xn 타일링/minji.js
@@ -4,11 +4,12 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 const n = +input[0];
 let dp = Array.from({ length: 1001 }, () => 0);
 
-dp[1] = 1n;
-dp[2] = 2n;
+dp[1] = 1;
+dp[2] = 2;
 
 for (let i = 3; i <= n; i += 1) {
-  dp[i] = dp[i - 1] + dp[i - 2];
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+  // dp 테이블에 너무 큰 값이 들어가지 않도록 10007을 나눈 값을 넣기
 }
 
-console.log(Number(dp[n]));
+console.log(dp[n]);
