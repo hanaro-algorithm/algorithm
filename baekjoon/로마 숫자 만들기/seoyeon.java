e0bd59f61e94fff81091e166e4bda49757d19fc2
seoyeon08 <silversun0808@gmail.com>
2024-08-21 20:22:59 +0900

    로마 숫자 만들기 최종

--- baekjoon/로마 숫자 만들기/seoyeon.java
+++ baekjoon/로마 숫자 만들기/seoyeon.java
@@ -5,17 +5,14 @@ public class Main {
     static int list[] = {1, 5, 10, 50};
     static boolean visit[] = new boolean[1001];    // 방문 여부
     static int answer = 0;
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
         int n = scan.nextInt(); // n값
         scan.close();
 
         dfs(n, 0, 0);
-        sb.append(answer);
-        System.out.println(sb);
+        System.out.println(answer);
     }
     private static void dfs(int n, int idx, int sum){
         if (n == 0){
