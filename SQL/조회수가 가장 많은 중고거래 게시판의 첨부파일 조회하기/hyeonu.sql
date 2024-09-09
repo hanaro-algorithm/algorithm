564a5d14b23d35204f946e1932b8bf90193adfad
khw18 <gmsb14@naver.com>
2024-09-09 22:02:31 +0900

    19주차 문제풀이

---
+++ SQL/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/hyeonu.sql
@@ -0,0 +1,5 @@
+select concat('/home/grep/src/', f.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT)
+from USED_GOODS_BOARD b
+         join USED_GOODS_FILE f on b.BOARD_ID = f.BOARD_ID
+where views = (select max(views) from USED_GOODS_BOARD)
+order by FILE_ID desc
