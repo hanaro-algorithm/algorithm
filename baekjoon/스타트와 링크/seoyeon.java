a9f2aac763f00aa17b2b6e56943a1f89b1e2daa3
seoyeon08 <silversun0808@gmail.com>
2024-09-11 21:10:08 +0900

    modify

--- baekjoon/스타트와 링크/seoyeon.java
+++ baekjoon/스타트와 링크/seoyeon.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
     static int N, answer = Integer.MAX_VALUE;
-    static int[][] players;
-    static boolean[] selected;
+    static int[][] players;         // 점수표
+    static boolean[] selected;      // 방문여부
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -26,31 +26,33 @@ public class Main {
 
     }
     static void solve(int depth, int index) {
+        // 팀 조합이 완성되면
         if (depth == N/2) {
             int startTeam = 0;
             int linkTeam = 0;
 
-            for (int i = 0; i < N - 1; i++) {
+            for (int i = 0; i < N - 1; i++) {           // 방문한 팀과, 방문하지 않은 팀을 나누어, 각각의 시너지 값들 구하기
                 for (int j = i + 1; j < N; j++) {
-                    if (selected[i] && selected[j]) {
+                    if (selected[i] && selected[j]) {   // i, j 번째 사람이 true라면 startTeam에 +
                         startTeam += players[i][j];
                         startTeam += players[j][i];
-                    } else if (!selected[i] && !selected[j]) {
+                    } else if (!selected[i] && !selected[j]) {  // false라면 linkTeam에 +
                         linkTeam += players[i][j];
                         linkTeam += players[j][i];
                     }
                 }
             }
 
-            int diff = Math.abs(startTeam - linkTeam);
-            answer = Math.min(answer, diff);
+            int diff = Math.abs(startTeam - linkTeam);  // 차이값
+            answer = Math.min(answer, diff);            // 최솟값
             return;
         }
-        for (int i = index; i < N; i++) {
+        for (int i = index; i < N; i++) {   // true, false로 팀 나누기
+            // 만약 방문하지 않았다면,
             if (!selected[i]) {
-                selected[i] = true;
-                solve(depth + 1, i + 1);
-                selected[i] = false;
+                selected[i] = true;         // 방문으로 변경하고
+                solve(depth + 1, i + 1);    // 재귀 호출
+                selected[i] = false;        // 재귀 끝나면 방문 false로 바꿔주기
             }
         }
     }
