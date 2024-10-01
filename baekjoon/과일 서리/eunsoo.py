13f8d7ac376be621582740e8c99ebf8db3ee4624
EunSo0 <dmstn423@naver.com>
2024-10-01 16:39:12 +0900

    add: 과일서리 수정

--- baekjoon/과일 서리/eunsoo.py
+++ baekjoon/과일 서리/eunsoo.py
@@ -1,8 +1,15 @@
-import math
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+for i in range(m):
+    dp[1][i+1] = 1
 
-print(math.comb(m-1,n-1))
+for i in range(1,n+1):
+    dp[i][i] = 1
+    for j in range(i+1,m+1):
+        dp[i][j] = dp[i-1][j-1] + dp[i][j-1]
+
+print(dp[-1][-1])
