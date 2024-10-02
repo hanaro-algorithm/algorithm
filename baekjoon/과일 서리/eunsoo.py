0e5e76bc3c58e38d91593bdfff1499ee89f763ac
EunSo0 <dmstn423@naver.com>
2024-10-02 15:47:34 +0900

    add: 과일서리 수정2

--- baekjoon/과일 서리/eunsoo.py
+++ baekjoon/과일 서리/eunsoo.py
@@ -1,11 +1,17 @@
+import math
 import sys
+from itertools import combinations_with_replacement
+
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
+
+# 1. 조합
+print(math.comb(m-1,m-n))
+
+# 2. DP
 dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
-for i in range(m):
-    dp[1][i+1] = 1
 
 for i in range(1,n+1):
     dp[i][i] = 1
