f6108de8df447a03837fb9c4591d04c8f39b0175
minjilee <budlalswl1129@gmail.com>
2024-12-23 11:10:52 +0900

    전공책 문제 풀이 수정

--- baekjoon/전공책/minji.js
+++ baekjoon/전공책/minji.js
@@ -8,41 +8,41 @@ for (let i = 2; i < N + 2; i += 1) majorBooks.push(input[i].split(' '));
 
 // 만들고자 하는 단어 개수 해시맵에 넣기
 const wordMap = new Map();
-T.forEach((t) => {
-  wordMap.set(t, !wordMap.get(t) ? 1 : wordMap.get(t) + 1);
-});
+T.forEach((t) => wordMap.set(t, (wordMap.get(t) || 0) + 1));
 
+majorBooks.sort((a, b) => +a[0] - +b[0]); // 전공책 가격 오름차순 정렬
 let answer = Number.MAX_SAFE_INTEGER;
 const visited = Array.from({ length: N }, () => false);
-const dfs = (index, selected, cost) => {
-  const map = new Map();
-  // 선택된 전공책 제목 해시맵에 저장
-  selected.forEach((item) => {
-    if (map.has(item)) map.set(item, map.get(item) + 1);
-    else map.set(item, 1);
-  });
-  let cnt = 0;
+const dfs = (index, selectedMap, cost) => {
+  if (cost >= answer) return; // 현재 비용이 최소 비용 이상이면 종료
+  let isAvailable = true;
   // 선택한 전공책에서 단어 만들 수 있는지 확인
-  [...wordMap.keys()].forEach((word) => {
-    if (map.get(word) >= wordMap.get(word)) cnt += 1;
-  });
-  if (cnt === wordMap.size) {
+  for (const [alphabet, count] of wordMap.entries()) {
+    if ((selectedMap.get(alphabet) || 0) < count) {
+      isAvailable = false;
+      break;
+    }
+  }
+  // 만들 수 있는 경우 최소값 갱신
+  if (isAvailable) {
     answer = Math.min(answer, cost);
     return;
   }
   for (let book = index + 1; book < N; book += 1) {
-    if (!visited[book] && majorBooks[book][1] !== '') {
+    if (!visited[book]) {
       visited[book] = true;
-      dfs(
-        book,
-        selected.concat(...majorBooks[book][1]),
-        cost + +majorBooks[book][0]
-      );
+      const [price, title] = majorBooks[book];
+      const newMap = new Map(selectedMap);
+      // 기존 해시맵에 새로운 전공책 제목 추가
+      for (const t of title) {
+        newMap.set(t, (newMap.get(t) || 0) + 1);
+      }
+      dfs(book, newMap, cost + +price);
       visited[book] = false;
     }
   }
 };
 
-dfs(-1, [], 0);
+dfs(-1, new Map(), 0);
 
 console.log(answer === Number.MAX_SAFE_INTEGER ? -1 : answer);
