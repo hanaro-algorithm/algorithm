488fecff6028028593f4c9806b7a1b942a1a9284
MyeongJi_Shin <tlsaudwl1821@naver.com>
2025-04-23 19:12:29 +0900

    2번 문제 수정

--- baekjoon/정사각형/tlsaudwl.java
+++ baekjoon/정사각형/tlsaudwl.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+        int T = sc.nextInt();
 
-        for (int i = 0; i < t; i++) {
+        for (int i = 0; i < T; i++) {
             int[] x = new int[4];
             int[] y = new int[4];
 
@@ -14,26 +14,27 @@ public class Main {
                 y[j] = sc.nextInt();
             }
 
-            List<Integer> distances = new ArrayList<>();
+            int[] distances = new int[6];
+            int idx = 0;
+
             for (int j = 0; j < 4; j++) {
                 for (int k = j + 1; k < 4; k++) {
                     int dist = (x[j] - x[k]) * (x[j] - x[k]) + (y[j] - y[k]) * (y[j] - y[k]);
-                    distances.add(dist);
+                    distances[idx++] = dist;
                 }
             }
 
-            Collections.sort(distances);
+            Arrays.sort(distances);
 
-            if (distances.get(0).equals(distances.get(1)) &&
-                    distances.get(1).equals(distances.get(2)) &&
-                    distances.get(2).equals(distances.get(3)) &&
-                    distances.get(4).equals(distances.get(5)) &&
-                    !distances.get(0).equals(distances.get(4))) {
+            if (distances[0] == distances[1] &&
+                    distances[1] == distances[2] &&
+                    distances[2] == distances[3] &&
+                    distances[4] == distances[5] &&
+                    distances[0] != distances[4]) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
         }
-        sc.close();
     }
 }
